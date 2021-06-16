89642e9e908a081e1c2415df67ec3b79647fff2e
Jessyca430 <moag6k@yahoo.fr>
2021-06-16 09:17:33 +0200

    finito adesso

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,14 +1,21 @@
 // define a function that will check if I can vote or not
 const canIVote = age => {
+
     // define a variable legalAge
+    
+    const legalAge = 18
 
     // create if else statement that console.log() if you can vote or not
-    if (condition) {
-        
-    } else {
-        
+    if (age >= legalAge ) {
+        console.log("you can Vote");   
+    } else { 
+        console.log("you can not Vote");   
+
     }
 }
+ canIVote(23)
+
+ canIVote(13)
 
 // define a variable for your age
 
