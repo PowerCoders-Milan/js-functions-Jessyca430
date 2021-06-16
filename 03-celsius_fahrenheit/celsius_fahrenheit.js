b7a0cae08a83b4415ae117de336277951afee7d6
Jessyca430 <moag6k@yahoo.fr>
2021-06-16 17:30:20 +0200

    eyi

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,20 @@
 // define a function multiplyByNineFifths(number)
+function multiplyByNineFifths(number) {
 
-    //return number*(9/5)
+    return number*(9/5)
+}
+
+function getFahrenheit(celsius) {
+return multiplyByNineFifths(celsius) + 32;
+}
+console.log(`${getFahrenheit(10)}`)
 
 // define a function getFahrenheit(celsius)
 
     // return multiplyByNineFifths(celsius) + 32;
 
-console.log() // call getFahrenheit();
+//console.log(getFahrenheitResult); // call getFahrenheit();
 
 
 // do the same thing starting from Fahrenheit to Celsius
+
